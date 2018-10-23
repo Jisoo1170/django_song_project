f0b7d34e399da643a6ea78d78b2149b1b34567a6
jisoo <jisoo1170@naver.com>
2018-10-23 20:48:07 +0900

    redirect로 수정

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     url(r'^song_write/$', song_write, name="song_write"),
     url(r'^song_add/$', song_add, name="song_add"),
+    url(r'^song_added/(?P<song_id>\d+)/$', song_added, name="song_added"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,23 +4,8 @@ from django.db import IntegrityError
 
 
 def song_write(request):
-    if request.method == 'GET':
-        return render(request, 'song/song_write.html')
-    # else:
-    #     # title = request.POST.get('title')
-    #     # singer = request.POST.get('singer')
-    #     # message = request.POST.get('message')
-    #     # store = request.POST.get('store')
-    #     # result = song.objects.create(title=title, singer=singer, message=message, store_id=int(store))
-
-    #     # request.session['temp_data'] = form.cleaned_data
-    #     # web_input = {'title': title, 'singer': singer, 'store': store}
-
-    #     # return render(request, 'greetings/greetings.html', web_input)
-    #     # return redirect('/song_add', web_input)
-    #     # return render(request, song_add, web_input)
-    #     return render(request, 'song/song_write.html')
-
+    return render(request, 'song/song_write.html')
+    
 def song_add(request):
     if request.method == 'GET':
         return render(request, 'song/song_write.html')
@@ -29,9 +14,10 @@ def song_add(request):
         singer = request.POST.get('singer')
         message = request.POST.get('message')
         store_id = int(request.POST.get('store'))
-        song.objects.create(title=title, singer=singer, message=message, store_id=store_id)
-        
-        delay_time = store.objects.get(pk=store_id).delay
+        new_song = song.objects.create(title=title, singer=singer, message=message, store_id=store_id)
+        return redirect("main:song_added", song_id=new_song.pk)
 
-        context = {"title": title, "singer": singer, "delay_time": delay_time}
-        return render(request, 'song/song_add.html', context)
+def song_added(request, song_id):
+    context = {'song' : song.objects.get(pk=song_id)}
+    return render(request, 'song/song_add.html', context)
+
