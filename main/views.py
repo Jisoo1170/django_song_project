1c12ed3f93792cf286385c15f6db39f8b5fa95d7
jisoo <jisoo1170@naver.com>
2018-10-24 00:36:01 +0900

    가게 - 리스트 보기,삭제,초기화 기능 구현

--- main/views.py
+++ main/views.py
@@ -1,12 +1,7 @@
-from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from .models import *
 from django.db import IntegrityError
 
-
-def song_write(request):
-    if request.method == 'GET':
-        return render(request, 'song/song_write.html')
-    
 def song_add(request):
     if request.method == 'GET':
         return render(request, 'song/song_write.html')
@@ -21,3 +16,17 @@ def song_add(request):
 def song_added(request, song_id):
     context = {'song' : song.objects.get(pk=song_id)}
     return render(request, 'song/song_add.html', context)
+
+def store_index(request, store_id):
+    context = {'song' : store.objects.get(pk=store_id).song_set.all, 'store_id' : store_id }
+    return render(request, 'song/store_index.html', context)
+
+def song_delete(request, song_id):
+    context = song.objects.get(pk=song_id)
+    context.delete()
+    store_id = context.store.id
+    return redirect('main:store_index', store_id=store_id)
+
+def song_all_delete(request, store_id):
+    store.objects.get(pk=store_id).song_set.all().delete()
+    return redirect('main:store_index', store_id=store_id)
